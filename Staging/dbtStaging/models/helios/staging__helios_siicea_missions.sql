d7040e3a97abec9449f834687f757b551fd69dc4
h-titouan <titouan.houde@seenovate.com>
2025-07-01 18:36:19 +0200

    Modify views with new columns name

--- Staging/dbtStaging/models/helios/staging__helios_siicea_missions.sql
+++ Staging/dbtStaging/models/helios/staging__helios_siicea_missions.sql
@@ -6,23 +6,23 @@ WITH missions AS (
     SELECT 
         *,
         CASE 
-            WHEN LENGTH("FINESS_géographique") = 8 THEN '0' || "FINESS_géographique"
-            ELSE "FINESS_géographique"
+            WHEN LENGTH("finess_geographique") = 8 THEN '0' || "finess_geographique"
+            ELSE "finess_geographique"
         END AS cd_finess
     FROM {{ ref('staging__sa_siicea_missions') }}
-    WHERE "Code_thème_IGAS" IN (
+    WHERE "code_theme_igas" IN (
         'MS634D13', 'MS634N1', 'MS634E1', 'MS634D12', 'MS634R1',
         'MS634D11', 'MS634D15', 'MS634C10'
     )
-    AND "Type_de_mission" NOT IN (
+    AND "type_de_mission" NOT IN (
         'Audit', 'Audit franco-wallon', 'Evaluation', 
         'Visites de conformité', 'Enquête administrative'
     )
-    AND "Statut_de_la_mission" IN ('Clôturé', 'Maintenu')
+    AND "statut_de_la_mission" IN ('Clôturé', 'Maintenu')
     AND (
-        CAST(SUBSTRING("Date_réelle__Visite_", 1, 4) || 
-             SUBSTRING("Date_réelle__Visite_", 6, 2) || 
-             SUBSTRING("Date_réelle__Visite_", 9, 2) AS INTEGER)
+        CAST(SUBSTRING("date_reelle_visite", 1, 4) || 
+             SUBSTRING("date_reelle_visite", 6, 2) || 
+             SUBSTRING("date_reelle_visite", 9, 2) AS INTEGER)
         BETWEEN 20220101 AND 20241231
     )
 )
