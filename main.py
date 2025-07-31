28a14844f1f4dfb40e746d47a7d7bdb16a53953d
@francois.lebrument <francois.lebrument@seenovate.com>
2025-07-31 12:46:11 +0200

    refactor : main and project.toml to feat with packages

--- main.py
+++ main.py
@@ -1,51 +1,51 @@
 # === Packages ===
-from datetime import date
-import argparse
 from typing import Literal
-from dotenv import load_dotenv
 
 # === Modules ===
-from pipeline.utils.load_yml import load_metadata_YAML
-from pipeline.utils.logging_management import setup_logger
+from pipeline.utils.config import env_var, setup_config
 from pipeline.pipeline_orchestration import (anais_staging_pipeline,
                                              local_staging_pipeline,
                                              anais_project_pipeline,
                                              local_project_pipeline)
 
-# === Paramètres ===
-today = date.strftime(date.today(), "%Y_%m_%d")
-load_dotenv()
-
-CHOICES = ["Staging", "Helios", "InspectionControlePA", "Matrice_PA", "Matrice_PH", "CertDC"]
-
 
 # === Fonction ===
-def main(env: Literal["local", "anais"], profile: str, profile_yml: str  = "profiles.yml", metadata_yml: str = "metadata.yml"):
+def main(config_var: dict):
     """
     Exécution de la Pipeline selon le profil indiqué.
 
     Parameters
     ----------
-    env : Literal["local", "anais"]
-        Nom de l'environnement sur lequel exécuter.
-    profile : str
-        Nom du profil à exécuter.
-    metadata : str, optional
-        Nom de fichier contenant les metadatas nécessaires au bon fonctionnement du projet, by default "metadata.yml"
+    config_var : dict
+        Dictionnaire contenant les configurations nécessaires à l'utilisation de la pipeline :
+            - liste des environnements possibles (env_choice)
+            - liste des projets existants (profile_choice)
+            - environnement (env)
+            - profile (profile)
+            - fichier de log (logger)
+            - information de gestion du projet (config)
+            - identifiants de la Database (db_config)
+            - identifiants de la Database Staging (staging_db_config)
     """
-    logger = setup_logger(env, f"logs/log_{env}.log")
-    config = load_metadata_YAML(metadata_yml, profile, logger, ".")
-    db_config = load_metadata_YAML(profile_yml, profile, logger, ".")["outputs"][env]
+    # Initialisation des configurations
+    config_var = setup_config(config_var)
+    profile_choice = config_var["profile_choice"]
+    env = config_var["env"]
+    profile = config_var["profile"]
+    logger = config_var["logger"]
+    config = config_var["config"]
+    db_config = config_var["db_config"]
+    staging_db_config = config_var["staging_db_config"]
+    today = config_var["today"]
 
+    # Exécution des pipelines dépendamment des paramètres d'entrées
     if profile == "Staging":
         if env == "anais":
             anais_staging_pipeline(profile, config, db_config, logger)
         if env == "local":
             local_staging_pipeline(profile, config, db_config, logger)
 
-    elif profile in CHOICES and profile != "Staging":
-        staging_db_config = load_metadata_YAML(profile_yml, "Staging", logger, ".")["outputs"][env]
-
+    elif profile in profile_choice and profile != "Staging":
         if env == "anais":
             anais_project_pipeline(profile, config, db_config, staging_db_config, today, logger)
         if env == "local":
@@ -54,9 +54,6 @@ def main(env: Literal["local", "anais"], profile: str, profile_yml: str  = "prof
 
 # === Exécution ===
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Exécution du pipeline")
-    parser.add_argument("--env", choices=["local", "anais"], default="local", help="Environnement d'exécution")
-    parser.add_argument("--profile", choices=CHOICES, default="Staging", help="Profile dbt d'exécution")
-    args = parser.parse_args()
+    config_var = env_var()
 
-    main(args.env, args.profile)
+    main(config_var)
