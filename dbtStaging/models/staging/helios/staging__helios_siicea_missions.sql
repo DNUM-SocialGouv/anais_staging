bf37781f879da0e5ff84bc5bee201247f03b27ca
@francois.lebrument <francois.lebrument@seenovate.com>
2025-07-31 17:06:50 +0200

    refactor : dbtStaging -> table with sa_siicea_missions change into real

--- dbtStaging/models/staging/helios/staging__helios_siicea_missions.sql
+++ dbtStaging/models/staging/helios/staging__helios_siicea_missions.sql
@@ -4,17 +4,21 @@
 
 WITH missions AS (
     SELECT 
-        *
-    FROM {{ ref('staging__sa_siicea_missions_real') }}
+        *,
+        CASE 
+            WHEN LENGTH(finess_geographique) = 8 THEN '0' || finess_geographique
+            ELSE finess_geographique
+        END AS cd_finess
+    FROM {{ ref('staging__sa_siicea_missions') }}
     WHERE code_theme_igas IN (
         'MS634D13', 'MS634N1', 'MS634E1', 'MS634D12', 'MS634R1',
         'MS634D11', 'MS634D15', 'MS634C10'
     )
-    AND type_mission NOT IN (
+    AND type_de_mission NOT IN (
         'Audit', 'Audit franco-wallon', 'Evaluation', 
         'Visites de conformité', 'Enquête administrative'
     )
-    AND statut_mission IN ('Clôturé', 'Maintenu')
+    AND statut_de_la_mission IN ('Clôturé', 'Maintenu')
     AND (
         CAST(SUBSTRING("date_reelle_visite", 1, 4) || 
              SUBSTRING("date_reelle_visite", 6, 2) || 
