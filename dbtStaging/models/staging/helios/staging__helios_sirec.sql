b0d845f94a988b78bc8832be3235b04a97810681
@francois.lebrument <francois.lebrument@seenovate.com>
2025-08-07 15:24:28 +0200

    fix : end date in staging__helios_sirec

--- dbtStaging/models/staging/helios/staging__helios_sirec.sql
+++ dbtStaging/models/staging/helios/staging__helios_sirec.sql
@@ -4,7 +4,7 @@
 
 WITH sirec AS (
     SELECT * FROM {{ ref('staging__sa_sirec') }}
-    WHERE date_de_reception_a_l_ars BETWEEN '2022-01-01' AND '2024-12-31'
+    WHERE date_de_reception_a_l_ars BETWEEN '2022-01-01' AND '2025-08-01'
 )
 
 SELECT * FROM sirec
