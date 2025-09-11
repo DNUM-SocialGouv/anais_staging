c336df9e4466c02ae76e631da935f8a33113e527
@francois.lebrument <francois.lebrument@seenovate.com>
2025-09-11 15:58:44 +0200

    fix : reduce_colname_size size to 63

--- dbtStaging/macros/reduce_colname_size.sql
+++ dbtStaging/macros/reduce_colname_size.sql
@@ -1,4 +1,4 @@
-{% macro reduce_colname_size(schema_name, colname, size=50) %}
+{% macro reduce_colname_size(schema_name, colname, size=63) %}
     {% if schema_name == 'public' %}
         {{ return(colname[:size].strip()) }}
     {% else %}
