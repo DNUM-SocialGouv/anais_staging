88556990a6b73a698125b0ab8174110cd643f65a
@francois.lebrument <francois.lebrument@seenovate.com>
2025-09-11 10:23:19 +0200

    test: back

--- dbtStaging/tests/generic/expect_column_to_exist.sql
+++ dbtStaging/tests/generic/expect_column_to_exist.sql
@@ -4,14 +4,14 @@ with actual_columns as (
     select column_name
     from information_schema.columns
     where lower(table_schema) = lower('{{ model.schema }}')
-      and lower(table_name) = lower({{ model.name }})
+      and lower(table_name) = lower('{{ model.name }}')
 )
 
-select {{ column_name }} as missing_column
+select '{{ column_name }}' as missing_column
 where not exists (
     select 1
     from actual_columns
-    where lower(column_name) = lower({{ column_name }})
+    where lower(column_name) = lower('{{ column_name }}')
 )
 
 {% endtest %}
