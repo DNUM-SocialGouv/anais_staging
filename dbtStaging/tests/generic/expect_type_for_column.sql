f42e7c405db3f605ce4bd9bfd32e5bf307131b71
@francois.lebrument <francois.lebrument@seenovate.com>
2025-09-11 11:37:05 +0200

    fix: expect_type_for_column to get data_type

--- dbtStaging/tests/generic/expect_type_for_column.sql
+++ dbtStaging/tests/generic/expect_type_for_column.sql
@@ -1,4 +1,4 @@
-{% test expect_type_for_column(model, column_name, expected_type) %}
+{% test expect_type_for_column(model, column_name) %}
    {% set type_mapping = {
         'INTEGER': ('integer', 'bigint'),
         'VARCHAR': ('varchar', 'text', 'character varying'),
@@ -6,24 +6,30 @@
         'DATE': ('date', 'date'),
         'BOOLEAN': ('bool', 'boolean')
     } %}
-{% set allowed_types = type_mapping[expected_type] %}
+    {% set expected_type = column.get('data_type') %}
 
-with actual_columns as (
+    {% if not expected_type %}
+        {% do exceptions.raise_compiler_error(
+            "Le champ 'data_type' est requis dans le schema.yml pour la colonne '" ~ column_name ~ "'"
+        ) %}
+    {% endif %}
+
+    with actual_columns as (
+        select
+            column_name,
+            lower(data_type) as data_type
+        from information_schema.columns
+        where lower(table_schema) = lower('{{ model.schema }}')
+        and lower(table_name) = lower('{{ model.name }}')
+    )
+
     select
-        column_name,
-        lower(data_type) as data_type
-    from information_schema.columns
-    where lower(table_schema) = lower('{{ model.schema }}')
-      and lower(table_name) = lower('{{ model.name }}')
-)
-
-select
-    '{{ column_name }}' as column_name,
-    data_type as actual_type,
-    '{{ expected_type }}' as expected_type,
-    'La colonne {{ column_name }} a un type différent de {{ expected_type }} (trouvé: ' || data_type || ')' as message
-from actual_columns
-where column_name = '{{ column_name }}'
-      and data_type not in {{ type_mapping[expected_type] }}
+        '{{ column_name }}' as column_name,
+        data_type as actual_type,
+        '{{ expected_type }}' as expected_type,
+        'La colonne {{ column_name }} a un type différent de {{ expected_type }} (trouvé: ' || data_type || ')' as message
+    from actual_columns
+    where column_name = '{{ column_name }}'
+        and data_type not in {{ type_mapping[expected_type] }}
 
 {% endtest %}
