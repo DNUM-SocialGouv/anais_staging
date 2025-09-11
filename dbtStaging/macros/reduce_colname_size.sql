7d3e011edb008abc6d0b15719ebe06b137770361
@francois.lebrument <francois.lebrument@seenovate.com>
2025-09-11 14:34:53 +0200

    fix : reduce_colname_size

--- dbtStaging/macros/reduce_colname_size.sql
+++ dbtStaging/macros/reduce_colname_size.sql
@@ -1,3 +1,3 @@
 {% macro reduce_colname_size(colname, size = 50) %}
-    {{ return("substr(" ~ colname ~ ", 1, " ~ size ~ ")") }}
+    {{ return(substr(" ~ colname ~ ", 1, " ~ size ~ ")) }}
 {% endmacro %}
