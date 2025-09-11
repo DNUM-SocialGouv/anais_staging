b88d1254d9600fd81aa5d774db0b035d708e8173
@francois.lebrument <francois.lebrument@seenovate.com>
2025-09-11 15:51:17 +0200

    test: 8

--- dbtStaging/macros/reduce_colname_size.sql
+++ dbtStaging/macros/reduce_colname_size.sql
@@ -1,5 +1,5 @@
-{% macro reduce_colname_size(schema, colname, size=50) %}
-    {% if schema == 'main' %}
+{% macro reduce_colname_size(schema_name, colname, size=50) %}
+    {% if schema_name == 'public' %}
         {{ return(colname[:size].strip()) }}
     {% else %}
         {{ return(colname.strip()) }}
