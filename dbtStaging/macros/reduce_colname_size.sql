52c311236c998f754ab53de106dd495fda615dfb
@francois.lebrument <francois.lebrument@seenovate.com>
2025-09-11 16:27:23 +0200

    fix: 7

--- dbtStaging/macros/reduce_colname_size.sql
+++ dbtStaging/macros/reduce_colname_size.sql
@@ -1,4 +1,4 @@
-{% macro reduce_colname_size(schema_name, colname, size=63) %}
+{% macro reduce_colname_size(schema_name, colname, size=61) %}
     {% if schema_name == 'public' %}
         {% set apostrophe_count = colname.count("''") %}
         {% set adjusted_size = size + apostrophe_count %}
