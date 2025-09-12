8e0880d941da7ce14d812a0aca432dfe620e02b3
@francois.lebrument <francois.lebrument@seenovate.com>
2025-09-12 15:47:48 +0200

    fix : 29

--- dbtStaging/macros/reduce_colname_size.sql
+++ dbtStaging/macros/reduce_colname_size.sql
@@ -1,29 +1,27 @@
 {% macro reduce_colname_size(schema_name, colname, size=63) %}
     {% if schema_name == 'public' %}
-        {% set col_str = colname | string | lower %}
+        {% set col_str = colname|string %}
         {# Compte les doubles apostrophes #}
         {% set apostrophe_count = col_str.count("''") %}
-        {% set accent_count = col_str | count("a") %}
         
-        -- {# Liste des accents les plus fréquents #}
-        {% set accents = ['a','â','ä','é','è','ê','ë','î','ï','ô','ö','ù','û','ü','ç'] %}
-        {% set accent_count = 0 %}
+        {# Liste des accents les plus fréquents #}
+        {% set accents = ['à','â','ä','é','è','ê','ë','î','ï','ô','ö','ù','û','ü','ç'] %}
+        
+        {% set col_without_accents = col_str %}
         {% for a in accents %}
-            {% set col_str = col_str | replace(a, "") %}
+            {% set col_without_accents = col_without_accents | replace(a, "") %}
         {% endfor %}
-        -- {% set accent_count = 0 %}
-        -- {% for a in accents %}
-        --     {% if a in col_str %}
-        --         {% set accent_count = accent_count + col_str.count(a) %}
-        --     {% endif %}
-        -- {% endfor %}
         
-        {# Ajustement : +apostrophes, -accents #}
+        {% set accent_count = col_str|length - col_without_accents|length %}
+
+        {# Log pour debug #}
         {{ log("colname=" ~ col_str ~ " | apostrophes=" ~ apostrophe_count ~ " | accents=" ~ accent_count, info=True) }}
+
+        {# Ajustement : +apostrophes, -accents #}
         {% set adjusted_size = size + apostrophe_count - accent_count %}
         
-        {{ return(colname.strip()[:adjusted_size]) }}
+        {{ return(col_str.strip()[:adjusted_size]) }}
     {% else %}
-        {{ return(colname.strip()) }}
+        {{ return(colname|string).strip() }}
     {% endif %}
 {% endmacro %}
