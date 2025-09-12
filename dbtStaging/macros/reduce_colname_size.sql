62f72d4024abd1cc1e7dd0fdd12475eeb483e9f1
@francois.lebrument <francois.lebrument@seenovate.com>
2025-09-12 17:12:05 +0200

    fix : 44

--- dbtStaging/macros/reduce_colname_size.sql
+++ dbtStaging/macros/reduce_colname_size.sql
@@ -30,18 +30,12 @@
 {% macro reduce_colname_size(schema_name, colname, size=63) %}
     {% if schema_name == 'public' %}
         {% set col_str = colname|string %}
-
-        {# Ajustement : +apostrophes, -accents #}
-        {% set apostrophe_count = dbtStaging.count_double_apostophes(col_str[:size]) | int %}
-        {% set adjusted_size = size + apostrophe_count %}
-        {% set accent_count = dbtStaging.count_accent_characters(col_str[:adjusted_size]) | int %}
-        {% set adjusted_size = size - accent_count %}
-
-        {# Log pour debug #}
-        {{ log("colname=" ~ colname ~ " | apostrophes=" ~ apostrophe_count ~ " | accents=" ~ accent_count, info=True) }}
-
-        {{ colname.strip()[:adjusted_size] }}
+        {% set raw_prefix = col_str[:size] %}
+        {% set apostrophe_count = count_double_apostrophes(raw_prefix) | int %}
+        {% set accent_count = count_accent_characters(raw_prefix) | int %}
+        {% set adjusted_size = size + apostrophe_count - accent_count %}
+        {{ col_str.strip()[:adjusted_size] | trim }}
     {% else %}
-        {{ (colname|string).strip() }}
+        {{ colname|string|trim }}
     {% endif %}
 {% endmacro %}
