96603e94555109c8f1ddef90e70008134e6fb17d
@francois.lebrument <francois.lebrument@seenovate.com>
2025-09-12 14:57:15 +0200

    fix : 17

--- dbtStaging/macros/reduce_colname_size.sql
+++ dbtStaging/macros/reduce_colname_size.sql
@@ -1,14 +1,19 @@
 {% macro reduce_colname_size(schema_name, colname, size=63) %}
     {% if schema_name == 'public' %}
+        {# Compte les doubles apostrophes #}
         {% set apostrophe_count = colname.count("''") %}
         
-        {% set accents = ["à","â","ä","é","è","ê","ë","î","ï","ô","ö","ù","û","ü","ç"] %}
+        {# Liste des accents les plus fréquents #}
+        {% set accents = ['a','â','ä','é','è','ê','ë','î','ï','ô','ö','ù','û','ü','ç'] %}
         
         {% set accent_count = 0 %}
-        {% set colname_without_accent = colname | regex_replace("[^A-Za-z0-9]", "") %}
-        -- {% for a in accents %}
-        --     {% set accent_count = accent_count + colname.count(a) %}
-        {% set accent_count = (colname | length) - (colname_without_accent | length) %}
+        {% for a in accents %}
+            {% if a in colname %}
+                {% set accent_count = accent_count + colname.count(a) %}
+            {% endif %}
+        {% endfor %}
+        
+        {# Ajustement : +apostrophes, -accents #}
         {% set adjusted_size = size + apostrophe_count - accent_count %}
         
         {{ return(colname.strip()[:adjusted_size]) }}
