6de841762a2590859dd0a3f4a9d4f0eb3183fd20
@francois.lebrument <francois.lebrument@seenovate.com>
2025-09-12 12:22:15 +0200

    fix: 11

--- dbtStaging/macros/reduce_colname_size.sql
+++ dbtStaging/macros/reduce_colname_size.sql
@@ -1,13 +1,17 @@
 {% macro reduce_colname_size(schema_name, colname, size=63) %}
     {% if schema_name == 'public' %}
         {% set apostrophe_count = colname.count("''") %}
-        
         {% set col_lower = colname | lower %}
         
-{% set accent_count = (col_lower | regex_findall('[^\x00-\x7F]')) | length %}
+        {% set accents = ["à","â","ä","é","è","ê","ë","î","ï","ô","ö","ù","û","ü","ç"] %}
+        {% set accent_count = 0 %}
+        {% for a in accents %}
+            {% set accent_count = accent_count + (col_lower.count(a)) %}
+        {% endfor %}
         
         {% set adjusted_size = size + apostrophe_count - accent_count %}
-        
+        {{ log("colname=" ~ colname ~ " | apostrophes=" ~ apostrophe_count ~ " | accents=" ~ accent_count, info=True) }}
+
         {{ return(colname.strip()[:adjusted_size]) }}
     {% else %}
         {{ return(colname.strip()) }}
