09f2d0e8635a7173353a8ed588bcd6434086bea3
@francois.lebrument <francois.lebrument@seenovate.com>
2025-09-12 10:50:05 +0200

    fix: reduce_colname_size count accent as 2 char

--- dbtStaging/macros/reduce_colname_size.sql
+++ dbtStaging/macros/reduce_colname_size.sql
@@ -1,7 +1,17 @@
-{% macro reduce_colname_size(schema_name, colname, size=61) %}
+{% macro reduce_colname_size(schema_name, colname, size=63) %}
     {% if schema_name == 'public' %}
-        {% set apostrophe_count = colname.count("''") %}
-        {% set adjusted_size = size + apostrophe_count %}
+        {# Compte les doubles apostrophes #}
+        {% set accents = ['à','â','ä','é','è','ê','ë','î','ï','ô','ö','ù','û','ü','ç'] %}
+        
+        {% set accent_count = 0 %}
+        {% for a in accents %}
+            {% if a in colname %}
+                {% set accent_count = accent_count + colname.count(a) %}
+            {% endif %}
+        {% endfor %}
+        
+        {% set adjusted_size = size - accent_count %}
+        
         {{ return(colname.strip()[:adjusted_size]) }}
     {% else %}
         {{ return(colname.strip()) }}
