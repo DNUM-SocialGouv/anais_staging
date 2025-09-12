67df4b833d785559eb0cb9720ff9f1bb5292d341
@francois.lebrument <francois.lebrument@seenovate.com>
2025-09-12 15:29:23 +0200

    fix : 26

--- dbtStaging/macros/reduce_colname_size.sql
+++ dbtStaging/macros/reduce_colname_size.sql
@@ -3,16 +3,17 @@
         {% set col_str = colname | string | lower %}
         {# Compte les doubles apostrophes #}
         {% set apostrophe_count = col_str.count("''") %}
+        {% set accent_count = col_str.count("a") %}
         
-        {# Liste des accents les plus fréquents #}
-        {% set accents = ['a','â','ä','é','è','ê','ë','î','ï','ô','ö','ù','û','ü','ç'] %}
+        -- {# Liste des accents les plus fréquents #}
+        -- {% set accents = ['a','â','ä','é','è','ê','ë','î','ï','ô','ö','ù','û','ü','ç'] %}
         
-        {% set accent_count = 0 %}
-        {% for a in accents %}
-            {% if a in col_str %}
-                {% set accent_count = accent_count + col_str.count(a) %}
-            {% endif %}
-        {% endfor %}
+        -- {% set accent_count = 0 %}
+        -- {% for a in accents %}
+        --     {% if a in col_str %}
+        --         {% set accent_count = accent_count + col_str.count(a) %}
+        --     {% endif %}
+        -- {% endfor %}
         
         {# Ajustement : +apostrophes, -accents #}
         {{ log("colname=" ~ col_str ~ " | apostrophes=" ~ apostrophe_count ~ " | accents=" ~ accent_count, info=True) }}
