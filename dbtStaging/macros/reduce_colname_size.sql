3b4445c938fb2a494d97ff821f9832e56b115da2
@francois.lebrument <francois.lebrument@seenovate.com>
2025-09-12 11:51:39 +0200

    fix : 6

--- dbtStaging/macros/reduce_colname_size.sql
+++ dbtStaging/macros/reduce_colname_size.sql
@@ -4,11 +4,13 @@
         
         {% set accents = ['à','â','ä','é','è','ê','ë','î','ï','ô','ö','ù','û','ü','ç'] %}
         
-        {% set accent_count = 0 %}
+        
+        {% set colname_without_accent = colname}
         {% for a in accents %}
-            {% set accent_count = accent_count + colname.count(a) %}
+            {{ colname_without_accent = colname_without_accent | replace(a,"")}}
         {% endfor %}
         
+        {% set accent_count = (colname | length) - (colname_without_accent | length) %}
         {% set adjusted_size = size + apostrophe_count - accent_count %}
         
         {{ return(colname.strip()[:adjusted_size]) }}
