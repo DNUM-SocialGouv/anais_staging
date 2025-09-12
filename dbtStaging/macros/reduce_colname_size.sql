164f2c06f4c7ce9ed8a5cc17e9574708bf181e9f
@francois.lebrument <francois.lebrument@seenovate.com>
2025-09-12 14:53:53 +0200

    fix : 16

--- dbtStaging/macros/reduce_colname_size.sql
+++ dbtStaging/macros/reduce_colname_size.sql
@@ -5,7 +5,7 @@
         {% set accents = ["à","â","ä","é","è","ê","ë","î","ï","ô","ö","ù","û","ü","ç"] %}
         
         {% set accent_count = 0 %}
-        {% set colname_without_accent = colname | regex_replace("[^A-Za-z0-9]", "")}
+        {% set colname_without_accent = colname | regex_replace("[^A-Za-z0-9]", "") %}
         -- {% for a in accents %}
         --     {% set accent_count = accent_count + colname.count(a) %}
         {% set accent_count = (colname | length) - (colname_without_accent | length) %}
