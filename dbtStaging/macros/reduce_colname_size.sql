24653af3988c3bfd58c4d6a0710a25fedba54d0b
@francois.lebrument <francois.lebrument@seenovate.com>
2025-09-12 15:21:22 +0200

    fix : 23

--- dbtStaging/macros/reduce_colname_size.sql
+++ dbtStaging/macros/reduce_colname_size.sql
@@ -1,13 +1,22 @@
 {% macro reduce_colname_size(schema_name, colname, size=63) %}
     {% if schema_name == 'public' %}
-{% set col_str = colname|string %}
-{% set apostrophe_count = col_str.count("''") %}
-{% set accent_count = col_str.count('a') %}        
+        {% set col_str = colname | string | lower %}
+        {# Compte les doubles apostrophes #}
+        {% set apostrophe_count = colname.count("''") %}
+        
+        {# Liste des accents les plus fréquents #}
+        {% set accents = ["a","â","ä","é","è","ê","ë","î","ï","ô","ö","ù","û","ü","ç"] %}
+        
+        {% set accent_count = 0 %}
+        {% for a in accents %}
+            {% if a in colname %}
+                {% set accent_count = accent_count + colname.count(a) %}
+            {% endif %}
+        {% endfor %}
+        
+        {# Ajustement : +apostrophes, -accents #}
         {% set adjusted_size = size + apostrophe_count - accent_count %}
-        {% set col_str = colname|string %}
-{% set apostrophe_count = col_str.count("''") %}
-{% set accent_count = col_str.count('a') %}
-
+        
         {{ return(colname.strip()[:adjusted_size]) }}
     {% else %}
         {{ return(colname.strip()) }}
