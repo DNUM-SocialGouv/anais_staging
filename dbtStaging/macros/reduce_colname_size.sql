dca5467531f4a0589dc5924e3eb92b639eba4be4
@francois.lebrument <francois.lebrument@seenovate.com>
2025-09-12 12:19:35 +0200

    fix : 10

--- dbtStaging/macros/reduce_colname_size.sql
+++ dbtStaging/macros/reduce_colname_size.sql
@@ -3,14 +3,8 @@
         {% set apostrophe_count = colname.count("''") %}
         
         {% set col_lower = colname | lower %}
-        {% set accents = ["à","â","ä","é","è","ê","ë","î","ï","ô","ö","ù","û","ü","ç"] %}
         
-        {% set accent_count = 0 %}
-        {% for c in col_lower %}
-            {% if c | ord > 127 %}
-                {% set accent_count = accent_count + 1 %}
-            {% endif %}
-        {% endfor %}
+{% set accent_count = (col_lower | regex_findall('[^\x00-\x7F]')) | length %}
         
         {% set adjusted_size = size + apostrophe_count - accent_count %}
         
